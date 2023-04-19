19a1e40dd76131be25f868837df93916a0fedec6
Alla199707 <onoschenko20@gmail.com>
2023-04-19 22:30:58 +0300

    ++

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -5,30 +5,30 @@ export const Container = styled.div`
   max-width: 960px;
   margin: 0 auto;
   padding: 0 16px;
-  background-color: #eeeaea;
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   gap: 12px;
   padding: 8px 0;
-  margin-bottom: 15px;
+  margin-bottom: 16px;
   border-bottom: 1px solid black;
   > nav {
     display: flex;
-    gap: 15px;
   }
 `;
 
 export const StyledLink = styled(NavLink)`
   padding: 8px 16px;
-  border-radius: 5px;
+  border-radius: 4px;
   text-decoration: none;
-  color: white;
-  background-color: #3885e4;
+  color: black;
+  font-weight: 500;
+
   &.active {
-    background-color: #e6d738;
+    color: white;
+    background-color: orangered;
   }
 `;
